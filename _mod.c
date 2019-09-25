95acc7e790cffb972d45d1adc6dda20fbafe36ef
Jose Calderon <jose.calderon@holbertonschool.com>
2019-09-25 18:45:00 -0500

    Adding the _mod.c to the opcode.

---
+++ _mod.c
@@ -0,0 +1,28 @@
+#include "monty.h"
+/**
+ * _mod - function to get the module of
+ * the first two stack elements.
+ * @stack: the stack
+ * @line_number: The line number
+ *
+ * Return: void
+ */
+void _mod(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp;
+	int t;
+	(void)line_number;
+
+	if ((*stack) == NULL || T->len < 2)
+	{
+		mng_errs(ERROR_MUL);
+	}
+	tmp = (*stack);
+	while (tmp->prev != NULL)
+		tmp = tmp->prev;
+	t = tmp->next->n % tmp->n;
+	tmp->next->n = t;
+	tmp->next->prev = NULL;
+	(*stack) = tmp->next;
+	free(tmp);
+}
