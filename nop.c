b7d4d28181ee94e2f1c27459725e02bb3f2410d9
Jose Calderon <jose.calderon@holbertonschool.com>
2019-09-25 13:50:46 -0500

    Adding the nop.c to the opcode.

---
+++ nop.c
@@ -0,0 +1,11 @@
+#include "monty.h"
+/**
+ * nop - that not do nothing.
+ * @stack: the stack
+ * @line_number: The line number
+ *
+ * Return: void
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+}
