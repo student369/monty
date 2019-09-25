c61da0c6f150b11b7fa6c868d4f171ad0bb83ecc
Jose Calderon <jose.calderon@holbertonschool.com>
2019-09-25 10:41:27 -0500

    Adding a function push.c to add elements to the stack

---
+++ push.c
@@ -0,0 +1,45 @@
+#include "monty.h"
+/**
+ * push - add a value to the stack
+ * @stack: the stack
+ * @line_number: The line number
+ *
+ * Return: void
+ */
+void push(stack_t **stack, unsigned int line_number)
+{
+	stack_t *nw = NULL;
+	char *n;
+	(void)line_number;
+
+	if (T->toks[1] == NULL)
+		mng_errs(ERROR_PUSH);
+	else
+		n = T->toks[1];
+	if (are_digits(n) == 1)
+	{
+		nw = (stack_t *)malloc(sizeof(stack_t));
+		if (nw == NULL)
+			mng_errs(ERROR_MALLOC);
+	}
+	else
+		mng_errs(ERROR_PUSH);
+	nw->n = atoi(n);
+	add_stack(stack, nw);
+}
+
+/**
+ * add_stack - add a new value to the stack
+ * @stack: the stack
+ * @nw: The new element
+ *
+ * Return: void
+ */
+void add_stack(stack_t **stack, stack_t *nw)
+{
+	nw->prev = NULL;
+	nw->next = *stack;
+	if (*stack)
+		(*stack)->prev = nw;
+	(*stack) = nw;
+}
