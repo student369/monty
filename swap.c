d2d87fa487fa0b06a210ad375ccbc5250a1df9e2
Jose Calderon <jose.calderon@holbertonschool.com>
2019-09-25 13:06:51 -0500

    Adding the swap.c function to the opcode.

---
+++ swap.c
@@ -0,0 +1,26 @@
+#include "monty.h"
+/**
+ * swap - function to swap two stack elements.
+ * @stack: the stack
+ * @line_number: The line number
+ *
+ * Return: void
+ */
+void swap(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp;
+	int t;
+	(void)line_number;
+
+	if ((*stack) == NULL)
+	{
+		mng_errs(ERROR_SWAP);
+	}
+	tmp = (*stack);
+	while (tmp->prev != NULL)
+		tmp = (*stack)->prev;
+	t = tmp->n;
+	tmp->n = tmp->next->n;
+	tmp->next->n = t;
+	(*stack) = tmp;
+}
