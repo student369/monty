258248c3f8040be83042f9f8e38f985a519aad91
Jose Calderon <jose.calderon@holbertonschool.com>
2019-09-25 10:38:37 -0500

    Adding a find_opcode.c function to verify the existence of a command.

---
+++ find_opcode.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+/**
+ * find_opcode - function to manage opcodes and it's functions.
+ *
+ * Return: function needed.
+ */
+void (*find_opcode(void))(stack_t **stack, unsigned int line_number)
+{
+	char *opcode;
+	instruction_t *i;
+	static instruction_t ins[] = {
+		{"push", push}, {"pall", pall}, {NULL, NULL}
+	};
+	i = ins;
+	opcode = T->toks[0];
+	while (i->opcode && strncmp(i->opcode, opcode, strlen(opcode)))
+		i++;
+	if (!i->f)
+		mng_errs(ERROR_UNKNOWN);
+	return (i->f);
+}
