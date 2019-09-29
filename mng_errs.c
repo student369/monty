13288290a7bd86bb67ef06faf982b353c8a343ac
Jose Calderon <jose.calderon@holbertonschool.com>
2019-09-28 20:11:10 -0500

    [UPDATE]

--- mng_errs.c
+++ mng_errs.c
@@ -9,25 +9,25 @@ void mng_errs(int e)
 {
 	unsigned int n;
 	static char *const errs[] = {
-		"Error: malloc failed\n",
-		"USAGE: monty file\n",
+		"Error: malloc failed",
+		"USAGE: monty file",
 		"", "", "usage: push integer",
-		"can't pint, stack empty\n",
-		"can't pop an empty stack\n",
-		"can't swap, stack too short\n",
-		"can't add, stack too short\n",
-		"can't sub, stack too short\n",
-		"can't div, stack too short\n",
-		"division by zero\n",
-		"can't mul, stack too short\n",
-		"can't mod, stack too short\n",
-		"can't pchar, stack empty\n",
-		"can't pchar, value out of range\n"
+		"can't pint, stack empty",
+		"can't pop an empty stack",
+		"can't swap, stack too short",
+		"can't add, stack too short",
+		"can't sub, stack too short",
+		"can't div, stack too short",
+		"division by zero",
+		"can't mul, stack too short",
+		"can't mod, stack too short",
+		"can't pchar, stack empty",
+		"can't pchar, value out of range"
 	};
 	if (T)
 		n = T->ln;
 	if (e <= 1)
-		fprintf(stderr, "%s", errs[e]);
+		fprintf(stderr, "%s\n", errs[e]);
 	else if (e == 2)
 		fprintf(stderr, "Error: Can't open file %s\n", T->fname);
 	else if (e == 3)
